546632fdc4bea58c01446a35feb0254343977deb
decen.labs <decendao@gmail.com>
2023-03-25 15:15:53 +0800

    open zeppllin path

---
+++ ERC20Burnable.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.2/contracts/token/ERC20/extensions/ERC20Burnable.sol";
